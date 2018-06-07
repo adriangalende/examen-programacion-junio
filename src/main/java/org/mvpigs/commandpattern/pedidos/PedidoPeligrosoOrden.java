e53cc23245c01d0df2fcb2d14c834fc274eb1430
[adriangalende] <[adriangalende@gmail.com]>
2018-06-07 16:58:47 +0200

    TestComarca + test_pedido_peligroso_KO

--- src/main/java/org/mvpigs/commandpattern/pedidos/PedidoPeligrosoOrden.java
+++ src/main/java/org/mvpigs/commandpattern/pedidos/PedidoPeligrosoOrden.java
@@ -1,4 +1,26 @@
 package org.mvpigs.commandpattern.pedidos;
 
-public class PedidoPeligrosoOrden {
+
+import org.mvpigs.commandpattern.interfaces.PedidoPeligroso;
+
+public class PedidoPeligrosoOrden implements PedidoPeligroso {
+    private String destino;
+    private String instrucciones;
+
+    public PedidoPeligrosoOrden(String destino, String instrucciones){
+        this.destino = destino;
+        this.instrucciones = instrucciones;
+    }
+
+    public String instrucciones() {
+        return this.instrucciones;
+    }
+
+    public int peso() {
+        return 0;
+    }
+
+    public String destino() {
+        return this.destino;
+    }
 }
--- src/main/java/org/mvpigs/commandpattern/tratamientos/TratamientoPedidoPeligroso.java
+++ src/main/java/org/mvpigs/commandpattern/tratamientos/TratamientoPedidoPeligroso.java
@@ -1,4 +1,19 @@
 package org.mvpigs.commandpattern.tratamientos;
 
-public class TratamientoPedidoPeligroso {
+import org.mvpigs.commandpattern.interfaces.PedidoPeligroso;
+import org.mvpigs.commandpattern.interfaces.TratamientoPedido;
+
+public class TratamientoPedidoPeligroso implements TratamientoPedido {
+    PedidoPeligroso pedido;
+
+    public TratamientoPedidoPeligroso(PedidoPeligroso pedido){
+        this.pedido = pedido;
+    }
+
+    public boolean tratar() {
+        if(pedido.instrucciones().equals("no ponerselo en el dedo")){
+            return false;
+        }
+        return true;
+    }
 }
